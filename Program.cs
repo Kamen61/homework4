9b16826ed00281416e34aa7a5f58f4001d2ff4a8
Kamen <canen321@gmail.com>
2022-07-05 19:27:00 +0300

    exercise2

--- Program.cs
+++ Program.cs
@@ -3,22 +3,35 @@
 // 2, 4 -> 16
 
 
-System.Console.Write("Введите первое число : ");
-int a=Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите второе число : ");
-int b=Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите первое число : ");
+// int a=Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число : ");
+// int b=Convert.ToInt32(Console.ReadLine());
 
-int result=1;
+// int result=1;
 
-for (int i=0;i<b;i++){
-    result=result*a;
-}
-System.Console.WriteLine(result);
+// for (int i=0;i<b;i++){
+//     result=result*a;
+// }
+// System.Console.WriteLine(result);
+
+
+
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+
+System.Console.Write("Введите число : ");
+int number=Convert.ToInt32(Console.ReadLine());
+int result=0;
+while(number!=0){
+    result=result+number%10;
+    number=number/10;
+}
+System.Console.WriteLine(result);
 
 
 
